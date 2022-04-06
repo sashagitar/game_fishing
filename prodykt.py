3d68844ef7f2011c78065622d576b135685aa136
MaksimNikol <zverevmaksim55@gmail.com>
2022-04-06 18:48:35 +0300

    Загрузил программу

---
+++ prodykt.py
@@ -0,0 +1,19 @@
+from magaz import Magaz
+class Prodykt:
+    vid=str()
+    price=int()
+    stats=int()
+    id_ = int()
+    def __init__(self,id:str,vid:str,ves:int,price:int,stats:int):
+        self.vid= vid
+        self.ves= ves
+        self.stats = stats
+        self.price= price
+        self.id_ = id_
+    def __str__(self) -> str:
+        if id_ == 1:
+            return f"{self.vid} Цена:{self.price} Сон + {self.stats} " 
+        if id_ == 2:
+            return f"{self.vid} Цена:{self.price} Радость + {self.stats} " 
+        else:
+            return f"{self.vid} Цена:{self.price} Сытость + {self.stats} " 
