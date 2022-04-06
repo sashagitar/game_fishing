c44da3c442b1a5e7c0b5ffdd1e74357f8dc500c5
MaksimNikol <zverevmaksim55@gmail.com>
2022-04-06 19:03:39 +0300

    загрузил

---
+++ sadok.py
@@ -0,0 +1,22 @@
+import re
+from fish import Fish 
+class Sadok:
+    sadok=[]
+    n=int()
+    x=0
+    def __init__(self):
+        self.sadok = []
+        self.n = 0
+        self.cost=0
+    def Aad(self,f:Fish):
+        self.sadok.append(f)
+        self.n+=1
+        self.cost+=f.prise
+    def __str__(self,cost) -> str():
+        s=''
+        if self.n!=0:
+            for i in self.sadok:
+                s+=str(i)+'\n'
+            return s+'Общая стоимость: '+str(self.cost)+'\n'
+        else:
+            return 'Рыбы нет'
