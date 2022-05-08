c20d71570d6ec8e41c51ddc19d843e0745351677
seregakopylov <copylov.s3rgei@yandex.ru>
2022-05-08 10:26:50 +0300

    сон

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-from fish import Fish
+from prodykt import Prodykt
 from fishing import Fishing
 from holodos import Holodos
 from magaz import Magazin
 from pers import Pers
-from prodykt import Prodykt
 from sadok import Sadok
 
 FISHING = Fishing()
@@ -11,46 +10,122 @@ MAGAZ = Magazin()
 PERS = Pers()
 SADOK = Sadok()
 HOLODOS = Holodos()
+
+
 def fishing():
     print ('\n'*20)
-    if Fish != None:
-        sadok.Add(Fish)
-
-    pass
-
+    FISHING.go_fishing()
+    fish=FISHING.go_fishing()
+    if fish == None:
+        print('Эх соход')
+    else:
+        SADOK.add(fish)
+        print('Вы поймали :', fish)
+    input()
 def sadok():
+    print ('\n'*20)
     print(SADOK)
-    input('хотите продать рыбу')
-    if ('yes') :
-        PERS.money+=SADOK
-    pass
+    s = input('Вы хотететь продать кошке?')
+    if s== ('да') and ('возможно') and ('1110010011100000') and ('+'):
+        PERS.money+=SADOK.bue()
+    else:
+        print("Вы обидели кошку.(Пощады не ждите)")
 
-def eat():
-    pass
+
+def holodos():
+    print(HOLODOS)
+    tea=input('Вы берите еду')
+    if tea.isdigit() == True : 
+        product=Holodos.delete(int(tea))
+        if product != None:
+            if product.id_ == 0:
+                PERS.golod += product.stats
+            if product.id_ == 1:
+                PERS.son += product.stats
+            if product.id_ == 2:
+                PERS.happy += product.stats
+            else:
+                print("Ошибка")
+        else:
+            print('Вы ввели неверное число')
+    else:
+        print('Вы ввели неверное  не число!!')
+       
+        print('Вы накормили шлёпу',product)
+        input('\nНажмите ENTER, чтобы продолжить')
+    if tea.isnumeric() == False :
+        print('Вы ввели буквы!!!')
+        input('\nНажмите ENTER, чтобы продолжить')
+    else:
+        print('Вы ввели неверное число!!')
+        input('\nНажмите ENTER, чтобы продолжить')
 
 def magaz():
+    print ('\n'*20)
     print(MAGAZ)
-    vib=input('что хочешь купить')
+    vib = input("Выберите еду для Шлёпы :")
     if vib.isdigit():
-        prodykt=Magazin.bue(int(vib))
-        if prodykt!=None:
-            if PERS.money>Prodykt.price:
-                HOLODOS.Add(prodykt)
-                PERS.money==prodykt.price
+        product = MAGAZ.duy(int(vib))
+        if product !=None:
+            if PERS.money>=product.price:
+                PERS.money -= product.price
+                HOLODOS.add(product)
             else:
-                print('не достаточно денег')
+                print("У вас недостаточно средств")
         else:
-            print('вы ввели неправ число')
+            print('Вы ввели неверное число')
     else:
-        print('вы ввели не число')
+        print('Вы ввели неверное  не число!!')
 
-
-    pass
 
 def sleep():
+    print(PERS)
+    ot=input('хотите поспать?')
+    if (ot=='yes'):
+        PERS.son+=100
+        PERS.happy=100
     pass
 
 def happe():
+    print('''
+    Выберите действие:
+        1.Поиграть в футбол
+        2.Пойти в бар
+        3.Пойти в ночной клуб
+        4.Посмотреть телевизор
+    ''')
+    perehod = input('Выберите действие(НОМЕР!!!):\n')
+    try:
+        if (perehod == '') :
+            print('Введено неправлельное значение')
+        elif int(perehod) > 4 :
+            print('Введено неправлельное значение')
+    except ValueError:
+            print('Введено неправлельное значение')
+    if perehod == '1' :
+            ###Если день
+            PERS.sleep -= 10
+            PERS.golod -= 10
+            PERS.happy += 30
+    if perehod.lower() == '2' :
+        ###Если день
+            PERS.sleep -= 30
+            PERS.golod += 20
+            PERS.happy  += 10
+    if perehod.lower() == '3' :
+        ###Если день
+            PERS.sleep -= 10
+            PERS.golod -= 30
+            PERS.happy += 89
+    if perehod.lower() == '4' :
+        ###Если день
+            PERS.sleep -= 30
+            PERS.golod += 20
+            PERS.happy  += 10
+    input('\n\nНажмите ENTER, чтобы продолжить')
+    print('\n'*50)
+
+
     pass
 
 def finish():
@@ -60,24 +135,27 @@ def dayHaus():
     print ('\n'*20)
     pass
 
-def exet():
+def exit():
     print ('\n'*20)
-    w=input('Вы уверины что хотите выйти из игры?')
-    if w == 'да':
+    input('Вы уверны что хотите выйти:\n')
+    if ('да') and ('возможно') and ('1110010011100000'):
+        input('\nНажмите ENTER, чтобы продолжить')
         exit()
     else:
-        input('\нажмите ENTER, чтобы продолжить')
-    
-
+        input('\nНажмите ENTER, чтобы продолжить')
 
 def choise(go:str):
-    if go =='1':
-        fishing()
-    elif go=='2':
-        eat()
-    else:
-        print('вы ввели неверное действие')
-
+    swith = {
+        '1' : fishing,
+        '2' : holodos,
+        '3' : magaz,
+        '4' : sleep,
+        '5' : happe,
+        '6' : sadok,
+        '7' : dayHaus,
+        '8' : exit
+    }
+    swith.get(go, lambda: 'вы ввели не верное число')()
     
 
 def main():
@@ -91,20 +169,14 @@ def main():
     1. Порыбачить
     2. Покушать
     3. Рынок
-    4. Садок
-    5. Купить дом
-    6. Выход
+    4. Сон
+    5.
+    6. Садок
+    7. Купить дом
+    8. Выход
     ''')
     go = input('\n(Введите число или слово)\n')
-    switch = {
-        '1': fishing(),
-        '2': eat(),
-        '3': magaz(),
-        '4': sadok(),
-        '5': dayHaus(),
-        '6': exet(),
-    }
-    switch.get(go, '')
+    choise(go)
     main()
 
 def  start():
