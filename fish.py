6116c773b5a8b53026f1436671f7048b7f6b152d
MaksimNikol <101273673+MaksimNikol@users.noreply.github.com>
2022-05-11 19:46:03 +0300

    Add files via upload

--- fish.py
+++ fish.py
@@ -4,7 +4,7 @@ class Fish:
     ves=int()
     def __init__(self,vid:str,ves:int,price:int):
         self.vid= vid
-        self.ves= ves
+        self.ves = ves
         self.price= ves*price/1000
     def __str__(self) -> str:
         return f"{self.vid} Цена:{self.price} Вес:{self.ves} " 
